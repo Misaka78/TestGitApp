3d956ba809da22135cd9d3ad546bd6ad14c325e3
Maria <Maria@DESKTOP-2OU4NN7>
2022-01-18 12:18:42 +0200

    Мы создали наш новый класс

---
+++ TestGitApp/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitApp
+{
+    class Class1
+    {
+        // Это наш тестовый класс
+    }
+}
