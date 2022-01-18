f6192c9589dd6dad564b9c2e2da6533bd60eaaa3
Maria <Maria@DESKTOP-2OU4NN7>
2022-01-18 11:35:35 +0200

    Добавьте файлы проекта.

---
+++ TestGitApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine(Add(5,7));
+
+            Console.ReadKey();
+        }
+
+    }
+}
